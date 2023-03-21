41c6c9b4c40b537e6cd7a3b990490bfccd9f77ac
piccolo09 <keshavpachpinde3@gmail.com>
2023-03-21 08:19:53 +0530

    Added Urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from .views import HomeView,AboutusView
+from .views import HomeView,AboutusView,TopGView
 urlpatterns = [
-    path('/',HomeView.as_view(),name="Home"),
-    path('aboutus/',AboutusView.as_view(),name="AboutUs"),
+    path('',HomeView.as_view(),name="Home"),
+    path('aboutus/',AboutusView.as_view(),name="aboutus"),
+    path('topg/',TopGView.as_view(),name="TopG"),
 ]
