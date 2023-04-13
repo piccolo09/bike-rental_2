71bfed83f1b273562150981fc95a4427e2f8916d
piccolo09 <keshavpachpinde3@gmail.com>
2023-04-13 09:33:55 +0530

    SignIn & Signout Authentication

--- app/views.py
+++ app/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from django.views import View
+from django.contrib.auth import authenticate, login,logout
 
 # Create your views here.
+
+
+        
 
 class HomeView(View):
     def get(self,request):
         return render(request,"app/home.html",locals())
     
+    def post(self, request, **kwargs):
+        email = request.POST.get("SigninInputEmail")
+        password = request.POST.get("SigninInputPassword")
+        user_obj = authenticate(request,username=email,password=password)
+        if 'Logoutbutton' in request.POST:
+            print("LogOut call")
+            logout(request)
+            # return render(request,"app/home.html",{"isLogined":request.user.is_authenticated})
+            return HttpResponseRedirect("/")
+        elif "Signinbutton" in request.POST:
+            print("SignIn call")
+            if user_obj is not None:
+                login(request,user_obj)
+                return HttpResponseRedirect("/")
+                # return render(request,"app/home.html",{"isLogined":request.user.is_authenticated})
+            else:
+                return render(request,"app/home.html",{"isLogined":request.user.is_authenticated})
+        elif "Signupbutton" in request.POST:
+            print("SIngup")
+        
+
 class AboutusView(View):
     def get(self,request):
         return render(request,"app/aboutus.html",locals())
