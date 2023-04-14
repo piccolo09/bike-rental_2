cefddced0f963b94a01bb1392a25a64f12032841
piccolo09 <keshavpachpinde3@gmail.com>
2023-04-14 14:47:22 +0530

    MyProfile page & urls added

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView,AboutusView,TopGView,BikePageView
+from .views import HomeView,AboutusView,TopGView,BikePageView,MyProfileView
 urlpatterns = [
     path('',HomeView.as_view(),name="Home"),
     path('aboutus/',AboutusView.as_view(),name="aboutus"),
     path('topg/',TopGView.as_view(),name="TopG"),
     path('bikeview/',BikePageView.as_view(),name="bikeview"),
+    path('myprofile/',MyProfileView.as_view(),name="myprofile"),
+
 ]
