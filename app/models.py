9213ec9627908660e774858772f87a53b61c531d
piccolo09 <keshavpachpinde3@gmail.com>
2023-04-20 14:15:42 +0530

    basic changes

--- app/models.py
+++ app/models.py
@@ -1,72 +1,11 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager,UserManager,User,AbstractUser
-# class CustomUserManager(BaseUserManager):
-    
-#     def _create_user(self, email, password, **extra_feilds):
-#         if not email:
-#             raise ValueError("Email must be provider")
-#         if not password:
-#             raise ValueError("Password must be provider")
-
-#         user = self.model(
-#             email=self.normalize_email(email),
-#             **extra_feilds
-#         )
-#         user.set_password(password)
-#         user.save()
-#         return user
-    
-#     def create_user(self, email, password, **extra_fields):
-#         print("Create Called")
-#         extra_fields.setdefault("is_staff", False)
-#         extra_fields.setdefault("is_superuser", False)
-        
-#         return self._create_user(email, password, **extra_fields)
-
-#     def create_superuser(self, email, password, **extra_fields):
-#         extra_fields.setdefault("is_staff", True)
-#         extra_fields.setdefault("is_superuser", True)
-#         if extra_fields.get("is_staff") is not True:
-#             raise ValueError("Superuser must have is_staff=True.")
-#         if extra_fields.get("is_superuser") is not True:
-#             raise ValueError("Superuser must have is_superuser=True.")
-#         return self._create_user(email, password, **extra_fields)
-
-# # Create your models here.
-
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(unique=True, max_length=245)
-#     username = models.CharField(max_length=245,null=True)
-#     first_name = models.CharField(max_length=245,blank=True)
-#     last_name = models.CharField(max_length=245,blank=True)
-#     mobile = models.CharField(max_length=50,blank=True)
-#     address = models.TextField()
-
-#     is_superuser = models.BooleanField(default=False, verbose_name='superuser',blank=True)
-#     is_staff = models.BooleanField(default=False, verbose_name='staff',blank=True)
-#     is_active = models.BooleanField(default=True, verbose_name='active',blank=True)
-
-#     objects = CustomUserManager()
-
-#     USERNAME_FIELD = 'email'
-#     EMAIL_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name'] # Email & Password are required by default.
-
-#     class Meta:
-#         verbose_name = 'User'
-#         verbose_name_plural = 'Users'
-
-#     def __str__(self):
-#         return self.email
+from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
+    email = models.EmailField(unique=True)
     mobile = models.CharField(max_length=50,blank=True)
     address = models.TextField(blank=True)
-
-
-
 
 class BikeAd(models.Model):
     BIKETYPE_CHOICE = (
--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from .views import HomeView,AboutusView,TopGView,BikePageView,MyProfileView
 urlpatterns = [
-    path('',HomeView.as_view(),name="Home"),
+
+    #Home View
+    path('',HomeView.as_view(),name="home"),
+    
+    #About Us
     path('aboutus/',AboutusView.as_view(),name="aboutus"),
-    path('topg/',TopGView.as_view(),name="TopG"),
+    
+    #View Bike Individually
     path('bikeview/',BikePageView.as_view(),name="bikeview"),
+
+    #Individual User View
     path('myprofile/',MyProfileView.as_view(),name="myprofile"),
 
+    #Easter Egg
+    path('topg/',TopGView.as_view(),name="TopG"),
+
 ]
+
+
